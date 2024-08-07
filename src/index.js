2a6f28692cf5a8bb089043f336fc71ffd408dd1f
alinagunich2 <gunich.alina@gmail.com>
2024-08-07 16:17:51 +0300

    feat: add res

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let reverseString = String(n).split('').reverse()
+    if(reverseString[reverseString.length-1]==='-'){
+         reverseString.pop()
+        
+         return Number(reverseString.join(''))
+    }else{
+        return Number(reverseString.join(''))
+    }
 }
